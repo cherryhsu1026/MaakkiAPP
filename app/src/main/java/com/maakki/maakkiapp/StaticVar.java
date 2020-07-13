7d0b056bef3ef8e570fdc0ff078d64b9c845f9ad
cherryhsu1026 <cherryhsu1026@gmail.com>
2020-07-13 11:42:37 +0800

    將Maakki 網址改為http://47.91.185.13:8088/

--- app/src/main/java/com/maakki/maakkiapp/StaticVar.java
+++ app/src/main/java/com/maakki/maakkiapp/StaticVar.java
@@ -1,8 +1,8 @@
 package com.maakki.maakkiapp;
 
 public class StaticVar {
-    public static final String DomainName="47.91.185.13";
-    public static final String webURL="http://47.91.185.13/";
-    public static final String namespace="http://47.91.185.13/";
+    public static final String DomainName="47.91.185.13:8088";
+    public static final String webURL="http://47.91.185.13:8088/";
+    public static final String namespace="http://47.91.185.13:8088/";
     public static final String SPKey110 = "noticeRead";     //紀錄哪些公告ID 已讀過，不再顯示（中間以分號隔開）
 }
